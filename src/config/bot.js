af3bd99c0dea41d91f71a6e85fcba2b9c1f12075
delirio <43048579+idealizzare@users.noreply.github.com>
2026-06-05 06:51:33 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Listening to her",
+        name: "her",
         // Activity type number (0 = Playing).
         type: 2, 
       },
